0b26c7aec3f780d541d0a766435284d61da33f6a
Maksim Ermoshin <maksim_ermoshin@mail.ru>
2023-12-06 22:47:26 +0300

    Changed docker hub names

--- app/core/commands/drop_database.py
+++ app/core/commands/drop_database.py
@@ -13,7 +13,6 @@ async def drop_database() -> None:
 
     async with engine.begin() as connection:
         await connection.run_sync(Base.metadata.drop_all)
-        await connection.run_sync(Base.metadata.create_all)
 
 
 if __name__ == '__main__':
