78354bb8b9599a453dbda6f22b3cc1fb2125bbd6
Maksim Ermoshin <maksim_ermoshin@mail.ru>
2023-12-07 13:25:45 +0300

    Removed origin

--- app/main.py
+++ app/main.py
@@ -25,7 +25,6 @@ app.include_router(router_auth)
 app.include_router(router_users)
 
 origins = [
-    'http://127.0.0.1:8000',
     'http://localhost:3000',
     'http://localhost:5000',
     'http://localhost:5173',
