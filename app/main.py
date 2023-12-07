7f70e226164e60303905d6aa96994b9b7e265510
vlad3069 <vlad9603@gmail.com>
2023-12-07 16:43:18 +0300

    http

--- app/main.py
+++ app/main.py
@@ -39,6 +39,7 @@ origins = [
     'https://81.31.246.3:8000',
     'http://point-return.sytes.net',
     'https://point-return.sytes.net',
+    'http://point-return.github.io/',
     'https://point-return.github.io/',
 ]
 
